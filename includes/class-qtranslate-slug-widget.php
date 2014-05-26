6a0f7ed26d774b955a4b5d4bb91de6ffcef4b02b
Carlos Sanz García <carlos.sanz@gmail.com>
2014-05-26 18:09:51 +0200

    Updated file structure

---
+++ includes/class-qtranslate-slug-widget.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * QtranslateSlug_Widget class
+ *
+ * @since 1.0
+ */
+class QtranslateSlugWidget extends WP_Widget {
+     
+    function QtranslateSlugWidget() {
+        $widget_ops = array('classname' => 'qts_widget', 'description' => __('Allows your visitors to choose a Language.','qtranslate') );
+        $this->WP_Widget('qtranslateslug', __('Language selector', 'qts'), $widget_ops);
+    }
+    
+    function widget($args, $instance) {
+        extract($args);
+        
+        echo $before_widget;
+        $title = empty($instance['title']) ? __('Language', 'qtranslate') : apply_filters('widget_title', $instance['title']);
+        $hide_title = empty($instance['hide-title']) ? false : 'on';
+        $type = $instance['type'];
+        $short_text = ($instance['short_text'] == 'on') ? true : false ;
+        
+        if( $type!='text' && $type!='image' && $type!='both' && $type!='dropdown' ) $type='text';
+
+        if( $hide_title!='on')
+            echo $before_title . $title . $after_title;
+        
+        qts_language_menu($type, array( 'id' => $this->id, 'short' => $short_text ) );
+        
+        echo $after_widget;
+    }
+    
+    function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        $instance['title'] = $new_instance['title'];
+        $instance['hide-title'] = $new_instance['hide-title'];
+        $instance['type'] = $new_instance['type'];
+        $instance['short_text'] = $new_instance['short_text'];
+
+        return $instance;
+    }
+    
+    function form($instance) {
+        $instance = wp_parse_args( (array) $instance, array( 'title' => '', 'hide-title' => false, 'type' => 'text' ) );
+        $title = $instance['title'];
+        $hide_title = $instance['hide-title'];
+        $type = $instance['type'];
+        $short_text = $instance['short_text'];
+ ?>
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'qtranslate'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></label></p>
+        <p><label for="<?php echo $this->get_field_id('hide-title'); ?>"><?php _e('Hide Title:', 'qtranslate'); ?> <input type="checkbox" id="<?php echo $this->get_field_id('hide-title'); ?>" name="<?php echo $this->get_field_name('hide-title'); ?>" <?php echo ($hide_title=='on')?'checked="checked"':''; ?>/></label></p>
+        <p><?php _e('Display:', 'qtranslate'); ?></p>
+        <p><label for="<?php echo $this->get_field_id('type'); ?>1"><input type="radio" name="<?php echo $this->get_field_name('type'); ?>" id="<?php echo $this->get_field_id('type'); ?>1" value="text"<?php echo ($type=='text')?' checked="checked"':'' ?>/> <?php _e('Text only', 'qtranslate'); ?></label></p>
+        <p><label for="<?php echo $this->get_field_id('type'); ?>2"><input type="radio" name="<?php echo $this->get_field_name('type'); ?>" id="<?php echo $this->get_field_id('type'); ?>2" value="image"<?php echo ($type=='image')?' checked="checked"':'' ?>/> <?php _e('Image only', 'qtranslate'); ?></label></p>
+        <p><label for="<?php echo $this->get_field_id('type'); ?>3"><input type="radio" name="<?php echo $this->get_field_name('type'); ?>" id="<?php echo $this->get_field_id('type'); ?>3" value="both"<?php echo ($type=='both')?' checked="checked"':'' ?>/> <?php _e('Text and Image', 'qtranslate'); ?></label></p>
+        <p><label for="<?php echo $this->get_field_id('type'); ?>4"><input type="radio" name="<?php echo $this->get_field_name('type'); ?>" id="<?php echo $this->get_field_id('type'); ?>4" value="dropdown"<?php echo ($type=='dropdown')?' checked="checked"':'' ?>/> <?php _e('Dropdown Box', 'qtranslate'); ?></label></p>
+        <p><label for="<?php echo $this->get_field_id('short_text'); ?>"><?php _e('Show short name (en):', 'qts'); ?> <input type="checkbox" id="<?php echo $this->get_field_id('short_text'); ?>" name="<?php echo $this->get_field_name('short_text'); ?>" <?php checked($short_text, 'on')  ?>/></label></p>
+        <p><?php _e('Display:', 'qtranslate'); ?></p>
+        
+ <?php
+    }
+}
