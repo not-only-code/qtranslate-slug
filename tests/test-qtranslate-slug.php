15ca39bb3bc247b1e384dd10f72ab83b5a0a112d
Pedro de Carvalho <p@43.lc>
2017-03-12 00:31:31 +0000

    base tests and include default wp slug tests

--- tests/test-qtranslate-slug.php
+++ tests/test-qtranslate-slug.php
@@ -1,7 +1,19 @@
 <?php
+/**
+ * Class QtranslateSlug_Test
+ *
+ * @package Qtranslate_Slug
+ */
 
+/**
+ * Base tests for QTS test
+ */
 namespace QTS;
 class QtranslateSlug_Test extends \WP_UnitTestCase {
+
+	/**
+	 * check if qtranslate-slug is active
+	 */
 
 	function testActive() {
 		$this->assertTrue( ! empty( QTS_VERSION ), 'Is QtranslateSlug Plugin active?' );
