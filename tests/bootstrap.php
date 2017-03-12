dd3943c70217450326c4d981387c916857cf8cc1
Pedro de Carvalho <p@43.lc>
2017-03-12 00:27:47 +0000

    update and improve phpunit testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,26 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Qtranslate_Slug
+ */
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
 
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../qtranslate-slug.php';
+	$qts_dir = dirname( dirname( __FILE__ ) );
+	require $qts_dir . '/qtranslate-slug.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
-
--- tests/test-qtranslate-slug.php
+++ tests/test-qtranslate-slug.php
@@ -1,12 +1,10 @@
 <?php
 
-class QtranslateSlug_Test extends WP_UnitTestCase {
+namespace QTS;
+class QtranslateSlug_Test extends \WP_UnitTestCase {
 
 	function testActive() {
-
-        $this->assertTrue(true);
-        
-		//$this->assertTrue(is_plugin_active('qtranslate-slug/qtranslate-slug.php'), 'QtranslateSlug Plugin active');
+		$this->assertTrue( ! empty( QTS_VERSION ), 'Is QtranslateSlug Plugin active?' );
 	}
 }
 
